ac90c642be4947986d4e283fd6101a1be2fb8184
Stewart Allen <stewart@neuron.com>
2019-03-24 14:56:56 -0400

    switch from timelapse to re-spawning raspistill each time

--- start-camera.sh
+++ start-camera.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while /bin/true; do
-	raspistill -w 1600 -h 1200 -q 20 -n -o /tmp/camera.jpg -l /var/www/html/camera.jpg -tl 1000 -t 10000000 -ss 40000 -rot 90 || exit
+	raspistill -w 1600 -h 1200 -q 20 -n -o /tmp/camera.jpg -l /var/www/html/camera.jpg -t 500 -ss 40000 -rot 90 || exit
 done
