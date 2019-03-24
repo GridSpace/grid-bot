f9e875f990639c4d4ae32fc3191fafe60297fa89
Stewart Allen <stewart@neuron.com>
2019-03-24 17:57:37 -0400

    allow camera settings to be modified in real time

--- start-camera.sh
+++ start-camera.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cd /home/pi/grid-bot
+
 while /bin/true; do
-	raspistill -w 1600 -h 1200 -q 20 -n -o /tmp/camera.jpg -l /var/www/html/camera.jpg -t 500 -ss 40000 -rot 90 || exit
+    [ -f etc/camera.conf ] && source etc/camera.conf
+	raspistill -n \
+        -w ${WIDTH:-1600} \
+        -h ${HEIGHT:-1200} \
+        -q ${QUALITY:-20} \
+        -o ${FILE_TEMP:-/tmp/camera.jpg} \
+        -l ${FILE_PERM:-/var/www/html/camera.jpg} \
+        -t ${TIMEOUT:-500} \
+        -ss ${EXPOSURE:-40000} \
+        -rot ${ROTATION:-90} || exit
 done
