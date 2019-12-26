3f570496dad28f2782462932ff1e3616e875723d
Stewart Allen <stewart@neuron.com>
2019-12-26 17:16:59 -0500

    add ability to change serial options between restarts

--- start-gridbot.sh
+++ start-gridbot.sh
@@ -4,10 +4,12 @@ export HOME=/home/pi
 export ROOT=${HOME}/grid-bot
 export NODE=${ROOT}/node/bin/node
 export BAUD=250000
+export OPTS='--web --listen --baud=${BAUD} --filedir=${ROOT}/uploads'
 
 cd ${HOME}/grid-host
 while /bin/true; do
+    [ -f etc/serial.conf ] && source etc/serial.conf
 	echo "--- starting --- $(date)"
-	${NODE} src/serial.js --baud=${BAUD} --web --listen --filedir=${ROOT}/uploads
+	eval "${NODE} src/serial.js ${OPTS}"
 	echo "--- exited ---"
 done
