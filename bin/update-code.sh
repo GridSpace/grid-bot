3c45faf989f97cfe4d5b0ac039ed1c0314e77c2f
Stewart Allen <stewart@neuron.com>
2020-02-01 13:31:33 -0500

    make code update more robust

--- bin/update-code.sh
+++ bin/update-code.sh
@@ -2,10 +2,9 @@
 
 (
     echo "updating code from github"
-    cd ../grid-bot && git pull && npm i
-    cd ../grid-host && git pull && npm i
-    cd ../grid-apps && git pull && npm i
+    [ -d ../grid-bot ]  && (cd ../grid-bot  && git pull && npm i)
+    [ -d ../grid-host ] && (cd ../grid-host && git pull && npm i)
+    [ -d ../grid-apps ] && (cd ../grid-apps && git pull && npm i)
     sudo cp /home/pi/grid-bot/bin/root-rc.local /etc/rc.local
-
     echo "code update complete"
 ) | tee -a /tmp/update-code.log
