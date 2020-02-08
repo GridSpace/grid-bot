026f947256618b72b22ab01604af17ad5d836cbe
Stewart Allen <stewart@neuron.com>
2020-02-08 11:44:51 -0500

    no longer need to start gridhost. switching to grid.local for all devices

--- bin/start-root.sh
+++ bin/start-root.sh
@@ -3,5 +3,5 @@
 export ROOT=/home/pi/grid-bot
 
 nohup nice -n -20 su -l -c ${ROOT}/bin/start-gridbot.sh pi > /tmp/gridbot.log 2>&1 &
-nohup nice -n 19 su -l -c ${ROOT}/bin/start-gridhost.sh pi > /tmp/gridhost.log 2>&1 &
+#nohup nice -n 19 su -l -c ${ROOT}/bin/start-gridhost.sh pi > /tmp/gridhost.log 2>&1 &
 nohup nice -n 19 ${ROOT}/bin/start-camera.sh > /tmp/camera.log 2>&1 &
