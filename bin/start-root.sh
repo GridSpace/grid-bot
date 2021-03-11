91a182462f0903185d8a3cc211f7195a50b086df
Stewart Allen <stewart@neuron.com>
2021-03-11 10:31:35 -0500

    apply port port forward to wlan

--- bin/start-root.sh
+++ bin/start-root.sh
@@ -4,6 +4,7 @@ export ROOT=/home/pi/grid-bot
 
 # redirect traffic from port 80 to gridbot interface
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 4080
+iptables -A PREROUTING -t nat -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 4080
 
 nohup nice -n -20 su -l -c ${ROOT}/bin/start-gridbot.sh pi > /tmp/gridbot.log 2>&1 &
 #nohup nice -n 19 su -l -c ${ROOT}/bin/start-gridhost.sh pi > /tmp/gridhost.log 2>&1 &
