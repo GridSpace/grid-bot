f33bbeee1919b4cc307722ab7eda43db69eadf80
Stewart Allen <sa@grid.space>
2022-02-14 00:29:45 -0500

    newer libcamera libs too verbose

--- bin/start-root.sh
+++ bin/start-root.sh
@@ -9,4 +9,4 @@ iptables -A PREROUTING -t nat -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 4
 
 nohup nice -n -20 su -l -c ${ROOT}/bin/start-gridbot.sh pi > /tmp/gridbot.log 2>&1 &
 #nohup nice -n 19 su -l -c ${ROOT}/bin/start-gridhost.sh pi > /tmp/gridhost.log 2>&1 &
-nohup nice -n 19 ${ROOT}/bin/start-camera.sh > /tmp/camera.log 2>&1 &
+nohup nice -n 19 ${ROOT}/bin/start-camera.sh > /dev/null 2>&1 &
